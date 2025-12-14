530777b427d338fd0cf9125a0464ec6a4b4b8fb9
Parth.Bhatia <bhatiaparth119@gmail.com>
2025-12-14 07:32:17 +0530

    jwt and user auth config

--- src/main/java/com/vit/codechef/courtOfJustice/configuration/SecurityConfig.java
+++ src/main/java/com/vit/codechef/courtOfJustice/configuration/SecurityConfig.java
@@ -1,14 +1,37 @@
 package com.vit.codechef.courtOfJustice.configuration;
 
+import com.vit.codechef.courtOfJustice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * The type Security config.
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Autowired
+    private final UserRepository userRepository;
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
+    /**
+     * Filter chain security filter chain.
+     *
+     * @param http the http
+     * @return the security filter chain
+     * @throws Exception the exception
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
